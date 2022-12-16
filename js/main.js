0eb5380eadc8f4338a5b1c76e6d690cd2f54ad80
Michael Wood <113477758+mikewood17@users.noreply.github.com>
2022-12-16 14:55:12 +0000

    fixed issue with larger menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 // =================================
 
 const body = document.getElementsByTagName('body');
+const backgroundFade = document.querySelector('.cookie-consent');
 
 // slideout menu
 
@@ -15,12 +16,18 @@ function openMenu() {
     slide_menu.style.display = "block";
     main.style.marginRight = "300px";
     mainHeader.style.marginRight = "300px";
+    backgroundFade.style.display = 'block';
+    main.classList.toggle('menu-open');
+    body[0].style.overflow = 'hidden';
 }
 
 function closeMenu() {
     slide_menu.style.display = "none";
     main.style.marginRight = "0px";
     mainHeader.style.marginRight = "0px";
+    backgroundFade.style.display = 'none';
+    main.classList.toggle('menu-open');
+    body[0].style.overflow = 'scroll';
 }
 
 menu_btn.addEventListener('click', function() {
@@ -30,6 +37,13 @@ menu_btn.addEventListener('click', function() {
         openMenu();
     }
 })
+
+backgroundFade.addEventListener('click', function(){
+    if (slide_menu.style.display === 'block') {
+        closeMenu();
+    }
+})
+
 
 // sticky header
 
@@ -67,18 +81,21 @@ $(document).ready(function(){
 // cookie popup
 
 const cookieButton = document.querySelector('.cookie-button');
-const cookiePopup = document.querySelector('.cookie-popup')
+const cookiePopup = document.querySelector('.cookie-popup');
 
 function cookieGet(){
     cookiePopup.style.display = 'none';
+    backgroundFade.style.display = 'none';
     localStorage.setItem('cookies', 'accepted');
     body[0].style.overflow = 'scroll';
 }
 
 if (localStorage.getItem('cookies')){
     cookiePopup.style.display = 'none';
+    backgroundFade.style.display = 'none';
 } else {
     cookiePopup.style.display = 'grid';
+    backgroundFade.style.display = 'block';
     body[0].style.overflow = 'hidden';
 }
 
